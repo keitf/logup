6c4f59ee8f0bb581e69851da79013699a6270de2
sunzongll@163.com <sunzongll@163.com>
2019-12-02 19:18:36 +0800

    添加项目文件。

---
+++ logup/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace logup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "admin")
+            {
+                if (txtpassword.Text=="admin")
+                {
+                    Form2 form2 = new Form2();
+                    this.Hide();
+                    form2.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("密码错误！", "警告！",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("账号错误！", "警告！", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
